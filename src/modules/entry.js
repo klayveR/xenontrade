6cb94f48c8ec2a94e35c2cd4ecf97d17f47b3ec4
klayveR <tobiashoffmann7@gmail.com>
2018-09-01 18:26:29 +0200

    Switched to electron-forge to prepare for distribution

---
+++ src/modules/entry.js
@@ -0,0 +1,158 @@
+const Helpers = require("./helpers.js");
+
+class Entry {
+  /**
+  * Creates a new Entry object
+  *
+  * @constructor
+  * @param {XenonTrade} app A XenonTrade object to which the entries should be added to
+  * @param {number} id ID of the entry
+  */
+  constructor(app, id) {
+    this.app = app;
+    this.id = id;
+    this.template = "";
+    this.replacements = [];
+  }
+
+  /**
+  * Sets the template that should be used for this entry
+  *
+  * @param {string} template `.html` format template
+  */
+  setTemplate(template) {
+    this.template = template;
+  }
+
+  /**
+  * Sets the template that should be used for this entry
+  */
+  setReplacements(replacements) {
+    this.replacements = replacements;
+    this.replacements.push({find: "entry-id", replace: this.id});
+  }
+
+  /**
+  * Enables the close button on this entry
+  */
+  enableClose() {
+    var self = this;
+    var button = $(".entry[data-id='" + this.id + "']").find("#closeButton");
+
+    button.show().click(function(e) {
+      e.preventDefault();
+      self.close(true);
+    });
+  }
+
+  /**
+  * Enables a toggle
+  */
+  enableToggle(toggle) {
+    var self = this;
+    var button = $(".entry[data-id='" + this.id + "']").find("#" + toggle + "Button");
+
+    button.show().click(function(e) {
+      e.preventDefault();
+      self._toggle(toggle);
+    });
+  }
+
+  /**
+  * Toggles a toggle on this entry
+  */
+  _toggle(toggle) {
+    var icon = $(".entry[data-id='" + this.id + "']").find("#" + toggle + "Button").find("i");
+    icon.toggleClass("grey");
+
+    $("[data-" + toggle +  "='" + this.id + "']").toggleClass("hidden");
+    this.app.gui.updateWindowHeight();
+    this._onButtonClick();
+  }
+
+  /**
+  * Focuses Path of Exile on a button click
+  */
+  _onButtonClick() {
+    Helpers.focusPathOfExile();
+  }
+
+  /**
+  * Visualizes every trend on this entry
+  */
+  visualizeTrend() {
+    var trend = $(".entry[data-id='" + this.id + "']").find(".trend");
+
+    trend.peity("line");
+    this.app.gui.updateWindowHeight();
+  }
+
+  /**
+  * Enables auto close for this entry
+  */
+  enableAutoClose(seconds) {
+    var self = this;
+    var timeoutContainer = $(".entry[data-id='" + this.id + "']").find("#timeout");
+
+    if(seconds > 0) {
+      timeoutContainer.html(seconds);
+
+      var autoClose = setInterval(function() {
+        seconds--;
+        if(seconds < 99) {
+          timeoutContainer.html(seconds);
+        }
+        if (seconds === 0) {
+          clearInterval(autoClose);
+          self.close(false);
+        }
+      }, 1000);
+    }
+  }
+
+  /**
+  * Replaces replacements in entry and adds it to the GUI
+  */
+  add() {
+    var template = this._getReplacedTemplate(this.template, this.replacements, "%");
+
+    $(".main div:last-child").after(template);
+    this.app.gui.updateWindowHeight();
+  }
+
+  /**
+  * Closes and removes this entry
+  */
+  close(focusPathOfExile) {
+    $(".entry[data-id='" + this.id + "']").remove();
+    this.app.gui.updateWindowHeight();
+
+    if(focusPathOfExile) {
+      Helpers.focusPathOfExile();
+    }
+  }
+
+  /**
+  * Replaces replacements in template and returns it
+  *
+  * @return {Object}
+  */
+  _getReplacedTemplate(template, replacements, delimiter) {
+    for(var i = 0; i < replacements.length; i++) {
+      if(replacements[i].hasOwnProperty("replace") && replacements[i].hasOwnProperty("find")) {
+        // Format if value is float
+        if(Helpers.isFloat(replacements[i].replace)) {
+          replacements[i].replace = +parseFloat(replacements[i].replace).toFixed(2);
+        }
+
+        if(typeof template !== "undefined") {
+          template = template.replace(new RegExp(delimiter + replacements[i].find + delimiter, "g"), replacements[i].replace);
+        }
+      }
+    }
+
+    return template;
+  }
+}
+
+module.exports = Entry;
---
+++ src/modules/parser.js
@@ -0,0 +1,349 @@
+const itemVariants = require("./resource/itemVariants");
+const gemVariants = require("./resource/gemVariants");
+const clipboardItemTypes = require("./resource/clipboardItemTypes");
+const mapAffixes = require("./resource/mapAffixes");
+
+class Parser {
+  /**
+  * Creates a new Parser object
+  *
+  * @constructor
+  * @param {string} clipboard Content of the clipboard
+  */
+  constructor(clipboard) {
+    this.clipboard = clipboard;
+    this.data = {};
+  }
+
+  /**
+  * Returns `true` if clipboard content is item data from Path of Exile
+  *
+  * @returns {boolean}
+  */
+  isPathOfExileData() {
+    if(this.clipboard.startsWith("Rarity: ")) {
+      return true;
+    }
+
+    return false;
+  }
+
+  /**
+  * Returns an object containing data parsed from the clipboard
+  *
+  * @returns {Object}
+  */
+  parseData() {
+    this.data.rarity = this.getRarity();
+    this.data.type = this.getItemType();
+    this.data.name = this.getName();
+    this.data.identified = this.isIdentified();
+    this.data.links = this.getLinks();
+    this.data.relic = this.isRelic();
+    this.data.variant = this.getVariant();
+    this.data.stackSize = this.getStackSize();
+
+    return this.data;
+  }
+
+  /**
+  * Returns an object containing previously parsed data from the clipboard
+  *
+  * @returns {Object}
+  */
+  getData() {
+    return this.data;
+  }
+
+  /**
+  * Returns the variant of the clipboard item
+  *
+  * @returns {string}
+  */
+  getVariant() {
+    var variant = null;
+
+    if(this.data.type === "SkillGem") {
+      variant = this._getGemVariant();
+    } else
+    if(this.data.type === "Unique") {
+      variant = this._getUniqueVariant();
+    } else
+    if(this.data.type === "Map") {
+      variant = this._getMapVariant();
+    }
+
+    return variant;
+  }
+
+  /**
+  * Returns the variant of a map
+  *
+  * @returns {string}
+  */
+  _getMapVariant() {
+    return "Atlas2";
+  }
+
+  /**
+  * Returns the variant of a unique item
+  *
+  * @returns {string}
+  */
+  _getUniqueVariant() {
+    var variant = null;
+    var name = this.getName();
+
+    if(itemVariants.hasOwnProperty(name)) {
+      var match = this.clipboard.match(new RegExp(itemVariants[name].regex));
+
+      if(match) {
+        var value = match[1];
+        variant = itemVariants[name].matches[value];
+      }
+    }
+
+    return variant;
+  }
+
+  /**
+  * Returns the variant of a gem
+  *
+  * @returns {string}
+  */
+  _getGemVariant() {
+    var variant = "20"; // Default is level 20 gem
+    var name = this.getName();
+    var data = this.getGemData();
+    var corrupted = this.isCorrupted();
+
+    for(var i = 0; i < gemVariants.length; i++) {
+      if((name === gemVariants[i].name || gemVariants[i].name === null)
+        && (corrupted === gemVariants[i].corrupted || gemVariants[i].corrupted === null)
+        && data.level >= gemVariants[i].levelFrom
+        && data.level <= gemVariants[i].levelTo
+        && data.quality >= gemVariants[i].qualityFrom
+        && data.quality <= gemVariants[i].qualityTo) {
+          variant = gemVariants[i].variant;
+          break;
+        }
+      }
+
+      return variant;
+    }
+
+    /**
+    * Returns the rarity of the clipboard item
+    *
+    * @returns {string}
+    */
+    getRarity() {
+      var lines = this.getClipboardLines();
+      return lines[0].replace("Rarity: ", "");
+    }
+
+    /**
+    * Returns the name of the clipboard item
+    * For rare items, the base type is returned as the name
+    *
+    * @returns {string}
+    */
+    getName() {
+      var type = this.getItemType();
+      var rarity = this.getRarity();
+      var index = 1;
+
+      if(rarity === "Rare") {
+        index = 2;
+      }
+
+      var lines = this.getClipboardLines();
+      var name = lines[index].replace("<<set:MS>><<set:M>><<set:S>>", "");
+
+      if(type === "Map") {
+        name = this._removeMapAffixes(name);
+      }
+
+      return name;
+    }
+
+    /**
+    * Removes map affixes from normal and magic maps
+    *
+    * @returns {string}
+    */
+    _removeMapAffixes(name) {
+      // Remove prefixes and Superior
+      for(var i = 0; i < mapAffixes.prefix.length; i++) {
+        name = name.replace(mapAffixes.prefix[i] + " ", "");
+      }
+
+      // Remove suffixes
+      for(var j = 0; j < mapAffixes.suffix.length; j++) {
+        name = name.replace(" " + mapAffixes.suffix[j], "");
+      }
+
+      return name;
+    }
+
+    /**
+    * Returns the item type of the clipboard item
+    * Unique items are always returned as "Unique", regardless of base type
+    *
+    * @returns {string}
+    */
+    getItemType() {
+      var rarity = this.getRarity();
+      var type = rarity;
+
+      if(clipboardItemTypes.hasOwnProperty(rarity)) {
+        for(var i = 0; i < clipboardItemTypes[rarity].length; i++) {
+          var regex = new RegExp(clipboardItemTypes[rarity][i].regex);
+          var match = this.clipboard.match(regex);
+
+          if(match) {
+            type = clipboardItemTypes[rarity][i].type;
+            break;
+          }
+        }
+      }
+
+      return type;
+    }
+
+    /**
+    * Returns the stack size of a currency item
+    *
+    * @returns {number}
+    */
+    getStackSize() {
+      var stackSize = 1;
+      var regex = /Stack Size: ([0-9,]*)\/[0-9,]*/;
+      var stackSizeMatch = this.clipboard.match(regex);
+
+      if(stackSizeMatch) {
+        stackSize = stackSizeMatch[1].replace(",", "");
+      }
+
+      return stackSize;
+    }
+
+    /**
+    * Returns an object containing level and quality of a gem
+    *
+    * @returns {Object}
+    */
+    getGemData() {
+      var levelRegex = /Level: ([0-9]*)(?: \(Max\))?/;
+      var qualityRegex = /Quality: \+([0-9]*)%/;
+      var levelMatch = this.clipboard.match(levelRegex);
+      var qualityMatch = this.clipboard.match(qualityRegex);
+      var data = {level: 1, quality: 0};
+
+      if(levelMatch) {
+        data.level = parseInt(levelMatch[1]);
+      }
+
+      if(qualityMatch) {
+        data.quality = parseInt(qualityMatch[1]);
+      }
+
+      return data;
+    }
+
+    /**
+    * Returns the amount of connected links of the clipboard item
+    * The returned value can only be 0, 5 or 6
+    *
+    * @returns {number}
+    */
+    getLinks() {
+      var socketString = this._getSocketString();
+
+      // If the item has sockets...
+      if(socketString !== "") {
+        var sockets = socketString.split(" ");
+        var linkCount = (socketString.match(/-/g) || []).length;
+
+        // Count of links can be 5 max, so it's a 6L
+        if(linkCount === 5) {
+          return 6;
+        } else if(linkCount === 4) {
+          // Check if first or last link is missing, sockets.length === 3 because there's an empty element at the end
+          // Check if the length of the first or last string is 1, because if it was a 5l, one character (char) would be alone
+          if(sockets.length === 3 && (sockets[0].length === 1 || sockets[1].length === 1)) {
+            return 5;
+          }
+        }
+      }
+
+      return 0;
+    }
+
+    /**
+    * Returns the socket string of the clipboard item
+    *
+    * @returns {string}
+    */
+    _getSocketString() {
+      var lines = this.getClipboardLines();
+
+      for(var i = 0; i < lines.length; i++) {
+        if(lines[i].startsWith("Sockets:")) {
+          return lines[i].replace("Sockets: ", "");
+        }
+      }
+
+      return "";
+    }
+
+    /**
+    * Returns `true` if the item is identified
+    *
+    * @returns {boolean}
+    */
+    isIdentified() {
+      if(this.clipboard.includes("Unidentified")) {
+        return false;
+      }
+
+      return true;
+    }
+
+    /**
+    * Returns `true` if the item is a relic
+    *
+    * @returns {boolean}
+    */
+    isRelic() {
+      if(this.clipboard.includes("Relic Unique")) {
+        return true;
+      }
+
+      return false;
+    }
+
+    /**
+    * Returns `true` if the item is corrupted
+    *
+    * @returns {boolean}
+    */
+    isCorrupted() {
+      if(this.clipboard.includes("Corrupted")) {
+        return true;
+      }
+
+      return false;
+    }
+
+    /**
+    * Returns an array containing each line of the clipboard
+    *
+    * @returns {Array}
+    */
+    getClipboardLines() {
+      return this.clipboard.split("\n");
+    }
+  }
+
+  module.exports = Parser;
