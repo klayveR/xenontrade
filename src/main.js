de0f9298b7d85f0e3804419d46500925fd0622b3
klayveR <tobiashoffmann7@gmail.com>
2018-09-02 03:12:28 +0200

    Default handling of Squirrel events

--- src/main.js
+++ src/main.js
@@ -65,3 +65,5 @@ app.on("activate", () => {
 ipcMain.on("resize", function (e, w, h) {
   if(!debug) { win.setSize(Math.round(w), Math.round(h)); }
 });
+
+if(require('electron-squirrel-startup')) app.quit();
