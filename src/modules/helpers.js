4d358cbf0f2c07981bdc45c8a93df1b1d0cfbd50
klayveR <tobiashoffmann7@gmail.com>
2018-09-08 14:06:05 +0200

    Focus with ffi library

--- src/modules/helpers.js
+++ src/modules/helpers.js
@@ -111,21 +111,37 @@ class Helpers {
         console.error("Tried to focus Path of Exile but failed, either xdotool is not installed or Path of Exile is not running");
       });
     } else if(os.platform() === "win32") {
-      /**
-      * These were used for calling the python script directly, unfortunately that required python installed on the users system
-      *
-      * var scriptPath = Helpers.fixPathForAsarUnpack(path.join(__dirname, "../", "/resource/python/focus-window.py"));
-      * var scriptExecution = spawn("python", [scriptPath]);
-      */
-
-      var time = Date.now();
-      var scriptPath = Helpers.fixPathForAsarUnpack(path.join(__dirname, "../", "/resource/executables/focus-window.exe"));
-      var scriptExecution = spawn(scriptPath);
-
-      scriptExecution.on("exit", (code) => {
-        var took = Date.now() - time;
-        console.log("Focusing windows exited after " + took + "ms with code", code);
+      console.log("Trying to focus PoE via ffi");
+      var user32 = new ffi.Library('user32', {
+        'GetTopWindow': ['long', ['long']],
+        'FindWindowA': ['long', ['string', 'string']],
+        'SetActiveWindow': ['long', ['long']],
+        'SetForegroundWindow': ['bool', ['long']],
+        'BringWindowToTop': ['bool', ['long']],
+        'ShowWindow': ['bool', ['long', 'int']],
+        'SwitchToThisWindow': ['void', ['long', 'bool']],
+        'GetForegroundWindow': ['long', []],
+        'AttachThreadInput': ['bool', ['int', 'long', 'bool']],
+        'GetWindowThreadProcessId': ['int', ['long', 'int']],
+        'SetWindowPos': ['bool', ['long', 'long', 'int', 'int', 'int', 'int', 'uint']],
+        'SetFocus': ['long', ['long']]
       });
+
+      var kernel32 = new ffi.Library('Kernel32.dll', {
+        'GetCurrentThreadId': ['int', []]
+      });
+
+      var winToSetOnTop = user32.FindWindowA(null, "Path of Exile")
+      var foregroundHWnd = user32.GetForegroundWindow()
+      var currentThreadId = kernel32.GetCurrentThreadId()
+      var windowThreadProcessId = user32.GetWindowThreadProcessId(foregroundHWnd, null)
+      var showWindow = user32.ShowWindow(winToSetOnTop, 9)
+      var setWindowPos1 = user32.SetWindowPos(winToSetOnTop, -1, 0, 0, 0, 0, 3)
+      var setWindowPos2 = user32.SetWindowPos(winToSetOnTop, -2, 0, 0, 0, 0, 3)
+      var setForegroundWindow = user32.SetForegroundWindow(winToSetOnTop)
+      var attachThreadInput = user32.AttachThreadInput(windowThreadProcessId, currentThreadId, 0)
+      var setFocus = user32.SetFocus(winToSetOnTop)
+      var setActiveWindow = user32.SetActiveWindow(winToSetOnTop)
     }
   }
 
