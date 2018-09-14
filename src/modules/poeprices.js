bf4de7b3916078af74270ed32955b09fd28ceccb
klayveR <tobiashoffmann7@gmail.com>
2018-09-14 03:46:52 +0200

    Added more error handling for poeprices response

--- src/modules/poeprices.js
+++ src/modules/poeprices.js
@@ -1,5 +1,6 @@
 const Base64 = require('js-base64').Base64;
 const request = require("request-promise-native");
+const _ = require("underscore");
 
 class PoePrices {
   static request(itemText) {
@@ -8,13 +9,25 @@ class PoePrices {
       var url = "https://www.poeprices.info/api?l=" + config.get("league") + "&i=" + itemBase64;
 
       request(url, {json: true})
-      .then((price) => {
-        resolve({itemBase64, price});
+      .then((result) => {
+        if(!PoePrices.hasAllKeys(result)) {
+          reject(new Error("Request to <b>poeprices.info</b> was unsuccessful. Received an empty response."));
+        } else if(result.error !== 0) {
+          reject(new Error("Request to <b>poeprices.info</b> returned error code " + result.error + "."));
+        } else {
+          resolve({itemBase64, price: result});
+        }
       })
       .catch((error) => {
         reject(error);
       });
     });
+  }
+
+  static hasAllKeys(result) {
+    var requiredKeys = ["currency", "min", "max", "pred_explanation", "error"];
+
+    return _.every(requiredKeys, _.partial(_.has, result));
   }
 }
 
