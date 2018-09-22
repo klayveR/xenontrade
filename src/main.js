1f88b425c8a3c4eaf89eae8f14dfd9c55808af80
klayveR <tobiashoffmann7@gmail.com>
2018-09-22 23:22:56 +0200

    Using electron-window-manager for windows

--- src/main.js
+++ src/main.js
@@ -1,96 +1,99 @@
-const {app, Menu, BrowserWindow, Tray} = require("electron");
-const {ipcMain} = require("electron");
+const {app, Menu, BrowserWindow, Tray, ipcMain} = require("electron");
 const {autoUpdater} = require("electron-updater");
 const isDev = require('electron-is-dev');
 const log = require('electron-log');
-
+const windowManager = require('electron-window-manager');
 const Helpers = require("./modules/helpers.js");
 const path = require("path");
 const os = require("os");
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let win, tray;
-let config;
+let win;
+let config = Helpers.createConfig();
 let debug = false;
 
+// Disable auto download of updates
 autoUpdater.autoDownload = false;
+
+// Log file format
 log.transports.file.format = '[{d}/{m}/{y} {h}:{i}:{s}] [{level}] {text}';
 log.transports.console.format = '[{d}/{m}/{y} {h}:{i}:{s}] [{level}] {text}';
 
-let shouldQuit = app.makeSingleInstance(function(commandLine, workingDirectory) {
-  // Someone tried to run a second instance, we should focus our window.
-  if (win) {
-    if (win.isMinimized()) {
-      win.restore();
-    }
-    win.focus();
-  }
-});
-
-if (shouldQuit) {
-  app.quit();
-  return;
-}
-
-// Creates XenonTrade window
+// Create XenonTrade window
 function createWindow() {
-  config = Helpers.createConfig();
-
-  win = new BrowserWindow({
-    x: config.get("window.x"),
-    y: config.get("window.y"),
-    width: 300 * config.get("window.zoomFactor"),
-    height: 0,
-    frame: false,
-    backgroundThrottling: false,
-    skipTaskbar: true,
-    show: false,
-    transparent: true,
-    maximizable: false
+  windowManager.init({
+    'devMode': isDev ? true : false,
+    'appBase': "file://" + __dirname
   });
 
-  win.setResizable(false);
-  win.loadFile("./src/index.html");
-  win.setVisibleOnAllWorkspaces(true);
-  win.setFullScreenable(false);
+  win = windowManager.open('main', 'XenonTrade', '/index.html', false, {
+    'x': config.get("window.x"),
+    'y': config.get("window.y"),
+    'width': 300 * config.get("window.zoomFactor"),
+    'height': 0,
+    'frame': false,
+    'backgroundThrottling': false,
+    'skipTaskbar': true,
+    'show': false,
+    'transparent': true,
+    'maximizable': false,
+    'resizable': false,
+    'fullscreenable': false,
+    'alwaysOnTop': true
+  });
 
+  // Open dev tools when debug is enabled
   if(debug) {
-    win.setSize(800, 600);
-    win.webContents.openDevTools();
+    win.object.setSize(800, 600);
+    win.object.webContents.openDevTools();
   }
-
-  // Emitted when the window is closed
-  win.on("closed", () => {
-    win = null;
-  });
 
   // Emitted when the window is focused
-  win.on("focus", () => {
-    win.webContents.send('focus');
+  win.object.on("focus", () => {
+    win.object.webContents.send('focus');
   });
 
   // Emitted when the window is ready to be shown
-  win.on('ready-to-show', () => {
-    win.show();
+  win.object.on('ready-to-show', () => {
+    win.object.show();
   });
 }
 
-// Creates XenonTrade Tray
+// Create XenonTrade Tray
 function createTray() {
-  const iconPath = path.join(__dirname, '../', 'build/icon_512.png');
-  tray = new Tray(iconPath);
+  let iconPath = path.join(__dirname, '../', 'build/icon_512.png');
+  let tray = new Tray(iconPath);
+
   const contextMenu = Menu.buildFromTemplate([
-    { label: 'Show', click: function(){
-      win.show();
-    }},
-    { label: 'Close', click: function(){
-      win.close();
-    }}
+    {
+      label: 'Show', click: function() {
+        win.object.show();
+      }
+    },
+    {
+      label: 'Close', click: function() {
+        windowManager.closeAll();
+      }
+    }
   ]);
 
   tray.setToolTip('XenonTrade');
   tray.setContextMenu(contextMenu);
+}
+
+// Only allow one instance of XenonTrade
+let shouldQuit = app.makeSingleInstance(function(commandLine, workingDirectory) {
+  if(win) {
+    if(win.object.isMinimized()) {
+      win.object.restore();
+    }
+
+    win.object.focus();
+  }
+});
+
+if(shouldQuit) {
+  app.quit();
+  return;
 }
 
 // This method will be called when Electron has finished
@@ -100,6 +103,7 @@ app.on("ready", () => {
   createWindow();
   createTray();
 
+  // Check for updates if not in dev mode
   if (!isDev) {
     autoUpdater.checkForUpdates();
   }
@@ -110,14 +114,14 @@ app.on("window-all-closed", () => {
   app.quit();
 });
 
-// On update available, let browserWindow know
+// On update available, let main window know
 autoUpdater.on('update-available', (info) => {
-  win.webContents.send('update-available', info);
+  win.object.webContents.send('update-available', info);
 });
 
-// On update downloaded, let browserWindow know
+// On update downloaded, let main window know
 autoUpdater.on('update-downloaded', (info) => {
-  win.webContents.send('update-downloaded', info);
+  win.object.webContents.send('update-downloaded', info);
 });
 
 // Download update when receiving download-update
@@ -125,21 +129,23 @@ ipcMain.on("download-update", (event, arg) => {
   autoUpdater.downloadUpdate();
 });
 
-// When receiving a install-update, quit and install the new version
+// When receiving install-update, quit and install the new version
 ipcMain.on("install-update", (event, arg) => {
   autoUpdater.quitAndInstall();
 });
 
-// When receiving resize signal, resize browserWindow
-ipcMain.on("resize", function (e, w, h) {
-  // Save position before resizing and then apply position again
-  // The window would resize but try to keep it centered for one
-  // user, this should fix it
+// When receiving resize, resize the main window
+ipcMain.on("resize", function (ev, width, height) {
   if(!debug) {
-    var windowPosition = win.getPosition();
-    win.setSize(Math.round(w), Math.round(h));
+    // Save previous window position
+    let windowPosition = win.object.getPosition();
+
+    // Set new window size
+    win.object.setSize(Math.round(width), Math.round(height));
+
+    // Apply previous position again to fix up- and downwards resize on some Linux distros
     if(os.platform() === "linux") {
-      win.setPosition(windowPosition[0], windowPosition[1]);
+      win.object.setPosition(windowPosition[0], windowPosition[1]);
     }
   }
 });
